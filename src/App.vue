15a22e3a8fb984666c61029e2b66aa5cf7915bb4
maricm123 <mihailomaric001@gmail.com>
2022-11-05 23:44:08 +0100

    Added new components.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
 <template>
-<div class="">
-  <Nav/>
+    <div>
+        <Navbar />
 
-  <router-view/>
-
-  <Footer/>
-</div>
+        <section class="section">
+            <router-view/>
+        </section>
+    </div>
 </template>
 
 <script>
-import Footer from '@/components/Footer'
-import Nav from '@/components/Nav'
+    import axios from 'axios'
 
-export default {
-  
-}
+    import Navbar from '@/components/layout/Navbar'
+
+    export default {
+        name: 'App',
+        components: {
+            Navbar
+        },
+        beforeCreate() {
+            this.$store.commit('initializeStore')
+
+            if (this.$store.state.token) {
+                axios.defaults.headers.common['Authorization'] = "Token " + this.$store.state.token
+            } else {
+                axios.defaults.headers.common['Authorization'] = ""
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
+@import '../node_modules/bulma';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,36 +1,67 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import store from '../store'
+
 import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+
 import SignUp from '../views/SignUp.vue'
 import LogIn from '../views/LogIn.vue'
+import Dashboard from '../views/dashboard/Dashboard.vue'
+import MyAccount from '../views/dashboard/MyAccount.vue'
 
 const routes = [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: About
-    },
-    {
-      path: '/signup',
-      name: 'SignUp',
-      component: SignUp
-    },
-    {
-      path: '/login',
-      name: 'LogIn',
-      component: LogIn
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      requireLogin: true
     }
-  ]
-
+  },
+  {
+    path: '/dashboard/my-account',
+    name: 'MyAccount',
+    component: MyAccount,
+    meta: {
+      requireLogin: true
+    }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+// if some of these routes above have argument require login true
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/log-in')
+  } else {
+    next()
+  }
 })
 
 export default router
